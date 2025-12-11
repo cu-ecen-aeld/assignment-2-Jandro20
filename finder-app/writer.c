b92dc921e51e4b59191ed3fbe737cef40b58bdc6
Alejandro Hidalgo Muñoz de Rivera <ahidalgo@oesia.com>
2025-12-11 10:59:05 +0100

    add writer.c/makefile and update finder-test.sh

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,24 +1,31 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <syslog.h>
 
 
+// First argument: Filename
+// Second argument: String to write
 int main(int argc, char *argv[]) {
 
+    openlog(NULL, 0, LOG_USER);
+
     if(argc!= 3) {
-        printf("Usage: %s <number1> <number2>\n", argv[0]);
+        syslog(LOG_ERR, "Usage: %s <number1> <number2>\n", argv[0]);
         return 1;
     }
 
     FILE *fp = fopen(argv[1], "w");
     if(fp == NULL) {
-        printf("Error opening file: %s\n", argv[1]);
+        syslog(LOG_ERR, "Error opening file: %s\n", argv[1]);
         return 1;
     }
 
     fprintf(fp, "%s", argv[2]);
 
     fclose(fp);
+
+    syslog(LOG_DEBUG, "Writing %s to %s\n", argv[1], argv[2]);
 
     return 0;
 }
